729ea018970188cbcb1facc864c9ef4497efbb83
vagrant <asjstksmr@gmail.com>
2020-05-23 01:40:53 +0000

    [finish]4-4

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,7 @@
 class BlogsController < ApplicationController
   def index
+    # 記事を全件取得
+    @blogs = Blog.all
   end
 
   def show
@@ -14,11 +16,13 @@ class BlogsController < ApplicationController
 
   def create
   	blog = Blog.new(blog_params)
-  	blog.sava
+  	blog.save
   	redirect_to blogs_path
   end
+
+  private
   def blog_params
-  	params.require(:blog).premit(:title, :category, :body)
+  	params.require(:blog).permit(:title, :category, :body)
   end
   
 end
