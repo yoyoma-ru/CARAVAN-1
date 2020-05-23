e68d5d64cb434a4d5d41bbab68c255f490fc1ccf
vagrant <asjstksmr@gmail.com>
2020-05-23 00:40:20 +0000

    [finish]4-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,8 +6,19 @@ class BlogsController < ApplicationController
   end
 
   def new
+  	@blog = Blog.new
   end
 
   def edit
   end
+
+  def create
+  	blog = Blog.new(blog_params)
+  	blog.sava
+  	redirect_to blogs_path
+  end
+  def blog_params
+  	params.require(:blog).premit(:title, :category, :body)
+  end
+  
 end
